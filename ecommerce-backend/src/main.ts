7457ead263f4727c7b0b81400e96e040f71ce98f
jacobosanchezbrito <sbjacobo4@gmail.com>
2025-09-21 16:52:51 -0500

    Update main.ts

--- ecommerce-backend/src/main.ts
+++ ecommerce-backend/src/main.ts
@@ -6,12 +6,12 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'http://localhost:3000', // para pruebas locales
-      'mcis-3-front-jacobos-projects-647b1425.vercel.app', // tu front en Vercel
+      'http://localhost:3000', // local dev
+      'https://mcis-3-front-rjcjbwir1-jacobos-projects-647b1425.vercel.app', // vercel front
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true, // si manejas cookies o headers con credenciales
+    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
